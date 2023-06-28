51d89072b5a4264d1275c2976d7ee112e354c217
metehangurgentepe <metehangurgentepe@gmail.com>
2023-06-28 15:52:58 +0300

    Initial Commit

---
+++ chuckNorrisJokes/chuckNorrisJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  chuckNorrisJokesApp.swift
+//  chuckNorrisJokes
+//
+//  Created by Metehan Gürgentepe on 28.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct chuckNorrisJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
